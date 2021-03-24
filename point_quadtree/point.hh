9f6c7d7462328b4d920a65f4b85373b3da8eb7ce
rlee32 <rlee32@gatech.edu>
2021-03-24 16:27:35 -0400

    first impl of point

---
+++ point_quadtree/point.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace point_quadtree {
+
+struct Point {
+    double x_{0};
+    double y_{0};
+
+    double distance(const Point &other) const;
+
+    // returns true if points are the same (distance is 0).
+    bool is_coincident(const Point &other) const;
+};
+
+} // namespace point_quadtree
