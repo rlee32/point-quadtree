a8a34dc62acbba4235ed38c7cd17d53366c2a161
rlee32 <rlee32@gatech.edu>
2021-03-24 16:38:10 -0400

    initial example impl

---
+++ example.cc
@@ -0,0 +1,57 @@
+#include "nano_timer.hh"
+#include "config.hh"
+#include "fileio.hh"
+
+#include <filesystem>
+#include <iostream>
+#include <optional>
+#include <string>
+
+int main(int argc, const char** argv)
+{
+    if (argc == 1) {
+        std::cout << "Arguments: config_file_path" << std::endl;
+    }
+
+    // Read config file.
+    const std::string config_path = (argc == 1) ? "config.txt" : argv[1];
+    std::cout << "Reading config file: " << config_path << std::endl;
+    Config config(config_path);
+
+    // Read input files.
+    const std::optional<std::string> tsp_file_path_string = config.get("tsp_file_path");
+    if (not tsp_file_path_string) {
+        std::cout << "tsp_file_path not specified.\n";
+        return EXIT_FAILURE;
+    }
+    const std::optional<std::filesystem::path> tsp_file_path(*tsp_file_path_string);
+    const auto [x, y] = fileio::read_coordinates(*tsp_file_path_string);
+    const auto initial_tour = fileio::initial_tour(x.size(), config.get("tour_file_path"));
+
+    // Make quad tree.
+    NanoTimer timer;
+    timer.start();
+
+    /*
+    std::cout << "\nquadtree stats:\n";
+    const auto &segments = make_segments(x, y, initial_tour);
+    auto quadtree = segment_quadtree::SegmentQuadtree(segments);
+    std::cout << "Finished quadtree in " << timer.stop() / 1e9 << " seconds.\n\n";
+
+    quadtree.validate();
+
+    int intersections{0};
+    int i{0};
+    for (const auto& segment : segments) {
+        // segment in the set should not 'intersect'.
+        if (quadtree.intersects(segment)) {
+            ++intersections;
+        }
+        std::cout << i << std::endl;
+        ++i;
+    }
+    std::cout << "Found " << intersections << " segments that intersect with others of " << segments.size() << " total." << std::endl;
+    */
+
+    return EXIT_SUCCESS;
+}
---
+++ makefile
@@ -0,0 +1,17 @@
+CXX = g++
+CXX_FLAGS = -std=c++17
+CXX_FLAGS += -Wuninitialized -Wall -Wextra -Werror -pedantic -Wfatal-errors # source code quality.
+CXX_FLAGS += -O3 -ffast-math # non-debug version.
+#CXX_FLAGS += -O0 -g # debug version.
+CXX_FLAGS += -I./ # include paths.
+
+SRCS = example.cc \
+    point_quadtree/point.cc
+
+%.o: %.cc; $(CXX) $(CXX_FLAGS) -o $@ -c $<
+
+OBJS = $(SRCS:.cc=.o)
+
+all: $(OBJS); $(CXX) $^ $(LINK_FLAGS) -o example.out
+
+clean: ; rm -rf example.out $(OBJS) *.dSYM
