535aff1515a48ee3fd6928a1d03aae1795758fb6
rlee32 <rlee32@gatech.edu>
2021-03-24 17:56:41 -0400

    add missing constants file

---
+++ point_quadtree/constants.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace point_quadtree {
+namespace constants {
+
+constexpr int MAX_TREE_DEPTH{15}; // maximum quadtree depth / level.
+
+} // namespace constants
+} // namespace point_quadtree
